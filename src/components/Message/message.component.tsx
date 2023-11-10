9d774a957534ed74cd748923c9946b832ba645e4
oThinas <thiagomartinsprado@gmail.com>
2023-11-10 13:29:00 -0300

    feat: modal

--- src/components/Message/message.component.tsx
+++ src/components/Message/message.component.tsx
@@ -1,16 +1,31 @@
 /** Core */
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { Modal, StyleSheet, TextInput, View } from 'react-native';
+import { Controller, useForm } from 'react-hook-form';
 
 /** Components */
-import { TextComponent } from '..';
+import { ButtonComponent, IconComponent, TextComponent } from '..';
 
 /** Props */
 import { IMessageProps } from './message.props';
 import { colors } from '../../core';
 
 export function MessageComponent(props: IMessageProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { control } = useForm<{ feedback: string }>();
+
   if (props.role === 'system') {
     return;
+  }
+
+  function handleOpenFeedbackModal() {
+    setIsModalOpen(true);
+    console.log('abrindo modal de feedback');
+  }
+
+  function handleSendFeedback() {
+    setIsModalOpen(false);
+    console.log('enviando feedback');
   }
 
   return (
@@ -18,6 +33,52 @@ export function MessageComponent(props: IMessageProps) {
       <TextComponent additionalStyles={styles.text}>
         {props.content}
       </TextComponent>
+
+      <View style={styles.reportButton}>
+        {props.role === 'assistant' && (
+          <ButtonComponent type='icon' onPress={() => handleOpenFeedbackModal()}>
+            <IconComponent iconName='MegaphoneSimple' weight='regular'/>
+          </ButtonComponent>
+        )}
+      </View>
+
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={isModalOpen}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.form}>
+            <TextComponent additionalStyles={{ textAlign: 'center' }}>
+              O que você achou da resposta?
+            </TextComponent>
+
+            <Controller
+              control={control}
+              name='feedback'
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  onChangeText={onChange}
+                  value={value}
+                  style={styles.field}
+                  multiline={true}
+                  numberOfLines={4}
+                  placeholder='Conte para nós seu feedback'
+                  placeholderTextColor={colors.gray}
+                />
+              )}
+            />
+
+            <ButtonComponent onPress={() => handleSendFeedback()}>
+              Enviar
+            </ButtonComponent>
+
+            <ButtonComponent additionalStyles={{ backgroundColor: colors.danger }} onPress={() => setIsModalOpen(false)}>
+              Cancelar
+            </ButtonComponent>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -38,4 +99,28 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
   },
   text: { fontSize: 20 },
+  reportButton: { alignSelf: 'flex-end' },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: `${colors.background.primary}75`,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    backgroundColor: colors.background.secondary,
+    borderRadius: 8,
+    padding: 16,
+    width: '80%',
+    gap: 16,
+  },
+  field: {
+    borderWidth: 2,
+    borderColor: colors.white,
+    borderRadius: 8,
+    padding: 8,
+    color: colors.white,
+    fontSize: 20,
+    width: '100%',
+    textAlignVertical: 'top',
+  },
 });
--- src/core/colors.ts
+++ src/core/colors.ts
@@ -4,6 +4,7 @@ export const colors = {
     secondary: '#230881',
   },
   white: '#F1F5F9',
+  gray: '#BABCBF',
   primary: '#4F46E5',
   danger: '#DC2626',
   success: '#0FC30C',
